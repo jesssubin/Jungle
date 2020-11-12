e2752b87d4fc2718473f1005f6191dff283de35f
jesssubin <jess.s.moon@gmail.com>
2020-11-12 19:48:47 +0000

    Add feature: user authentification

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 Rails.application.routes.draw do
 
+  # This route sends requests to our naked url to the *index* action in the *products* controller.
   root to: 'products#index'
+  
+  # I've created a gif controller so I have a page I can secure later. 
+  # This is optional (as is the root to: above).
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  # These routes will be for signup. The first renders a form in the browse, the second will 
+  # receive the form and create a user in our database using the data given to us by the user.
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
@@ -22,7 +35,8 @@ Rails.application.routes.draw do
     resources :categories
   end
 
-  
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
